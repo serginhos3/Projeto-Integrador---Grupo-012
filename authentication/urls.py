88d335105ef2f9b54dac4eb1abc5903142ad9f63
serginhos3 <sergiomagalhaesfilho@hotmail.com>
2024-05-17 23:00:02 -0300

    alterando arquivo de urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+# arquivo editado
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('signin/', views.signin, name="signin"),
